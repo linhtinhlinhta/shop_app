6f16cb4eff27db766497f6a7c000596d9ad93f2c
NguyenLinh99 <linhmt100899@gmail.com>
2020-03-10 10:39:31 +0700

    Fixed search ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :list_categories
+  before_action :set_search
   protect_from_forgery with: :exception
 
   def list_categories
     @categories = Category.all
+  end
+
+  def set_search
+    @q=Product.search(params[:q])
   end
 end
